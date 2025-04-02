3661979ab29a8d390544c8fd1dd1791a9f9c5170
ZZZZZ <1940315068@qq.com>
2025-04-02 12:12:19 +0800

    restructure tests

--- withNumpy/comparison.py
+++ withNumpy/comparison.py
@@ -2,10 +2,12 @@ import numpy as np
 from adal import adal_solver
 from irwa import irwa_solver
 from functions import exact_penalty_func, quadratic_form
+import osqp
+from scipy.sparse import csc_matrix
 import time
 
 
-scale = 5
+scale = 1
 n = 1000*scale  # number of variables
 m1 = 300*scale  # number of equality constraints
 m2 = 300*scale  # number of inequality constraints
@@ -55,30 +57,62 @@ end_adal = time.time()
 running_time_adal = end_adal-start_adal
 
 
+# Compare with osqp, l <= Ax <= u
+m = m1+m2
+u = np.zeros(m)
+l = np.zeros(m)
+A = csc_matrix(np.vstack([A_eq, A_ineq]))
+b = np.hstack([b_eq, b_ineq])
+P = csc_matrix(H)
+q = g
+
+# equality constraints: l = u = -b
+for i in range(m1):
+    l[i] = -b[i]
+    u[i] = -b[i]
+# inequality constraints, l = -inf, u = -b
+for i in range(m1,m):
+    l[i] = -np.inf
+    u[i] = -b[i]
+
+# start OSQP
+start_osqp = time.time()
+prob = osqp.OSQP()
+prob.setup(P=P, q=q, A=A, l=l, u=u)
+result_osqp = prob.solve()
+end_osqp = time.time()
+running_time_osqp = end_osqp-start_osqp
+x_osqp = result_osqp.x
+k_osqp = result_osqp.info.iter
+
 # compute function value with/without penalty
 val_irwa_penalty = exact_penalty_func(H, g, x_irwa, A_eq, b_eq, A_ineq, b_ineq)
 val_adal_penalty = exact_penalty_func(H, g, x_adal, A_eq, b_eq, A_ineq, b_ineq)
+val_osqp_penalty = exact_penalty_func(H, g, x_osqp, A_eq, b_eq, A_ineq, b_ineq)
 val_irwa_pri = quadratic_form(H, g, x_irwa)
 val_adal_pri = quadratic_form(H, g, x_adal)
+val_osqp_pri = quadratic_form(H, g, x_osqp)
 penalty_irwa = val_irwa_penalty - val_irwa_pri
 penalty_adal = val_adal_penalty - val_adal_pri
+penalty_osqp = val_osqp_penalty - val_osqp_pri
 
 # show the comparison
 print("------------------------------------------------------------")
-print(f"Number of variables: {n}")
-print(f"Number of constraints: {m1} + {m2} = {m1+m2}")
-print("------------------------------------------------------------")
 print(f"IRWA function value with penalty: {val_irwa_penalty:.6f}")
 print(f"ADAL function value with penalty: {val_adal_penalty:.6f}")
+print(f"OSQP function value with penalty: {val_osqp_penalty:.6f}")
 print("------------------------------------------------------------")
 print(f"IRWA function value without penalty: {val_irwa_pri:.6f}")
 print(f"ADAL function value without penalty: {val_adal_pri:.6f}")
+print(f"OSQP function value without penalty: {val_osqp_pri:.6f}")
 print("------------------------------------------------------------")
 print(f"IRWA penalty: {penalty_irwa:.3e}")
 print(f"ADAL penalty: {penalty_adal:.3e}")
+print(f"OSQP penalty: {penalty_osqp:.3e}")
 print("------------------------------------------------------------")
 print(f"IRWA running time: {running_time_irwa:.3f}s, iteration: {k_irwa}")
 print(f"ADAL running time: {running_time_adal:.3f}s, iteration: {k_adal}")
+print(f"OSQP running time: {running_time_osqp:.3f}s, iteration: {k_osqp}")
 print("------------------------------------------------------------")
 print(f"IRWA CG steps: {n_cg_steps_irwa}, CG total computation time: {time_cg_irwa:.4f}s")
 print(f"ADAL CG steps: {n_cg_steps_adal}, CG total computation time: {time_cg_adal:.4f}s")
--- withTorch/test.py
+++ withTorch/test.py
@@ -2,10 +2,7 @@ import torch
 from irwa import irwa_solver
 from adal import adal_solver
 import time
-from functions import *
-import osqp
-from scipy.sparse import csc_matrix
-import numpy as np
+from functions import exact_penalty_func, quadratic_form
 
 
 # Check GPU availability
@@ -15,9 +12,10 @@ torch.set_default_device(device)
 print(f"Using device: {device}")
 
 
-n = 2000  # number of variables
-m1 = 400  # number of equality constraints
-m2 = 400  # number of inequality constraints
+scale = 5
+n = 1000*scale  # number of variables
+m1 = 300*scale  # number of equality constraints
+m2 = 300*scale  # number of inequality constraints
 
 # equality constraints 
 A_eq = torch.rand(m1, n)
@@ -63,34 +61,6 @@ end_adal = time.time()
 running_time_adal = end_adal-start_adal
 
 
-# Compare with osqp, l <= Ax <= u
-m = m1+m2
-u = np.zeros(m)
-l = np.zeros(m)
-A = csc_matrix(torch.vstack([A_eq, A_ineq]).cpu().numpy())
-b = torch.hstack([b_eq, b_ineq]).cpu().numpy()
-P = csc_matrix(H.cpu().numpy())
-q = g.cpu().numpy()
-
-# equality constraints: l = u = -b
-for i in range(m1):
-    l[i] = -b[i]
-    u[i] = -b[i]
-# inequality constraints, l = -inf, u = -b
-for i in range(m1,m):
-    l[i] = -np.inf
-    u[i] = -b[i]
-
-# start OSQP
-start_osqp = time.time()
-prob = osqp.OSQP()
-prob.setup(P=P, q=q, A=A, l=l, u=u)
-result_osqp = prob.solve()
-end_osqp = time.time()
-running_time_osqp = end_osqp-start_osqp
-x_osqp = torch.from_numpy(result_osqp.x).to(device)  # Convert NumPy array to Torch tensor and move it to the device
-k_osqp = result_osqp.info.iter
-
 # compute function value with/without penalty
 val_irwa_penalty = exact_penalty_func(H, g, x_irwa, A_eq, b_eq, A_ineq, b_ineq)
 val_irwa_pri = quadratic_form(H, g, x_irwa)
@@ -98,26 +68,20 @@ val_irwa_pri = quadratic_form(H, g, x_irwa)
 val_adal_penalty = exact_penalty_func(H, g, x_adal, A_eq, b_eq, A_ineq, b_ineq)
 val_adal_pri = quadratic_form(H, g, x_adal)
 
-val_osqp_penalty = exact_penalty_func(H, g, x_osqp, A_eq, b_eq, A_ineq, b_ineq)
-val_osqp_pri = quadratic_form(H, g, x_osqp)
-
 
 # show the comparison
 print("------------------------------------------------------------")
 print(f"Number of variables: {n}")
-print(f"Number of constraints: {m1} + {m2} = {m}")
+print(f"Number of constraints: {m1} + {m2} = {m1+m2}")
 print("------------------------------------------------------------")
 print(f"IRWA function value with penalty: {val_irwa_penalty:.6f}")
 print(f"ADAL function value with penalty: {val_adal_penalty:.6f}")
-print(f"OSQP function value with penalty: {val_osqp_penalty:.6f}")
 print("------------------------------------------------------------")
 print(f"IRWA function value without penalty: {val_irwa_pri:.6f}")
 print(f"ADAL function value without penalty: {val_adal_pri:.6f}")
-print(f"OSQP function value without penalty: {val_osqp_pri:.6f}")
 print("------------------------------------------------------------")
 print(f"IRWA running time: {running_time_irwa:.3f}s, iteration: {k_irwa}")
 print(f"ADAL running time: {running_time_adal:.3f}s, iteration: {k_adal}")
-print(f"OSQP running time: {running_time_osqp:.3f}s, iteration: {k_osqp}")
 print("------------------------------------------------------------")
 print(f"IRWA CG steps: {n_cg_steps_irwa}, CG total computation time: {time_cg_irwa:.4f}s")
 print(f"ADAL CG steps: {n_cg_steps_adal}, CG total computation time: {time_cg_adal:.4f}s")
